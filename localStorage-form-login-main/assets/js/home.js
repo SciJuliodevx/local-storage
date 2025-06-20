b43558f658d7b2b189a834de53fc082f56750989
Julio Cardoso <julio.cardoso.ambrosio@gmail.com>
2025-06-20 18:58:13 -0300

    Delete localStorage-form-login-main directory

--- localStorage-form-login-main/assets/js/home.js
+++
@@ -1,7 +0,0 @@
-const btnlogout = document.getElementById("#btnLogout");
-
-const logout = () => {
- localStorage.removeItem("status")
-window.location.href = "../../index.html"}
-
-btnLogout.addEventListener("click", logout);
